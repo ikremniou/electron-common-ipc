47ac45141c81f8ac34565e9debf2a487e8793a0a
emmanuel.kimmerlin@thomsonreuters.com <emmanuel.kimmerlin@thomsonreuters.com>
2021-03-01 17:51:40 +0100

    Rename back IPC to Ipc

--- src/IpcBus/IpcBusClient-factory-browser.ts
+++ src/IpcBus/IpcBusClient-factory-browser.ts
@@ -1,14 +1,16 @@
 import { IpcBusClient } from './IpcBusClient';
-import { ElectronCommonIPCNamespace } from './renderer/IpcBusRendererPreload';
+import { ElectronCommonIpcNamespace, PreloadElectronCommonIpcAutomatic } from './renderer/IpcBusRendererPreload';
 
 const windowLocal = window as any;
 export const CreateIpcBusClient: IpcBusClient.CreateFunction = () => {
-    const electronCommonIPCSpace = windowLocal[ElectronCommonIPCNamespace];
-    if (electronCommonIPCSpace && electronCommonIPCSpace.CreateIpcBusClient) {
-        return electronCommonIPCSpace.CreateIpcBusClient();
+    const electronCommonIpcSpace = windowLocal[ElectronCommonIpcNamespace];
+    if (electronCommonIpcSpace && electronCommonIpcSpace.CreateIpcBusClient) {
+        return electronCommonIpcSpace.CreateIpcBusClient();
     }
     return null;
 }
 
 windowLocal.CreateIpcBusClient = CreateIpcBusClient;
 IpcBusClient.Create = CreateIpcBusClient;
+
+PreloadElectronCommonIpcAutomatic();
